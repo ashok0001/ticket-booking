a62081bc8ff6d340ba58fddb6a7b20223441dd8c
zoshcode <ashokzarmariya47@gmail.com>
2023-07-12 17:11:40 +0530

    add deployed api

--- ticket-booking/src/app/State/seat/Service.ts
+++ ticket-booking/src/app/State/seat/Service.ts
@@ -19,7 +19,7 @@ export class SeatService {
   loadAllSeats() {
     this.store.dispatch(loadSeats());
 
-    return this.http.get<Seat[]>(this.localHost).pipe(
+    return this.http.get<Seat[]>(this.apiUrl).pipe(
       map((seats: Seat[]) => loadSeatsSuccess({ seats })),
       catchError(error => of(loadSeatsFailure({ error })))
     ).subscribe(action => this.store.dispatch(action));
@@ -32,7 +32,7 @@ export class SeatService {
         'Content-Type': 'application/json',
       })
 
-    return this.http.put<Seat[]>(`${this.localHost}/book`,{numberOfSeats},{headers}).pipe(
+    return this.http.put<Seat[]>(`${this.apiUrl}/book`,{numberOfSeats},{headers}).pipe(
         map((seats:Seat[])=>bookSeatsSuccess({bookedSeats:seats})),
         catchError(error=>of(bookSeatsFailure({error})))
     ).subscribe(action=>{
@@ -44,7 +44,7 @@ export class SeatService {
   resetAllBookingHandler(){
     this.store.dispatch(resetAllBooking())
 
-    return this.http.put<Seat[]>(`${this.localHost}/reset-booking`,{}).pipe(
+    return this.http.put<Seat[]>(`${this.apiUrl}/reset-booking`,{}).pipe(
         map((seats:Seat[])=>resetAllBookingSuccess({seats})),
         catchError(error=>of(resetAllBookingFailure({error})))
     ).subscribe(action=>{
--- ticket-booking/src/app/booking/booking-form/booking-form.component.ts
+++ ticket-booking/src/app/booking/booking-form/booking-form.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, AbstractControl, ValidationErrors, AsyncValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { SeatService } from 'src/app/State/seat/Service';
 import { AppState } from 'src/app/model/AppState.model';
 import { Seat } from 'src/app/model/seat.model';
@@ -14,11 +14,14 @@ import { Seat } from 'src/app/model/seat.model';
 })
 export class BookingFormComponent {
 
+
+
   bookingForm: FormGroup=this.formBuilder.group({
     numberOfSeats: [null, Validators.required]
   });
  
  bookedSeats$:Observable<Seat[]>;
+ error:String='';
  
 
   
@@ -32,13 +35,19 @@ export class BookingFormComponent {
 
 
   handleSubmit=()=>{
-    if (this.bookingForm.valid) {
-      this.seatService.bookSeatsHandler(this.bookingForm.value.numberOfSeats)
-      console.log('The number is: ', this.bookingForm.value);
-    } else {
-      console.log('The number is invalid.');
-      // console.log('The number is: ', this.bookingForm.value);
+
+    if(this.bookingForm.value.numberOfSeats>7){
+      this.error="you can book max 7 seat at time"
+      return;
     }
+    else if(this.bookingForm.value.numberOfSeats<1 || !this.bookingForm.valid){
+      this.error="please enter valid number"
+      return;
+    }
+    
+    this.seatService.bookSeatsHandler(this.bookingForm.value.numberOfSeats)
+    this.error=''
+   
   }
 
   resetAllBooking=()=>{
